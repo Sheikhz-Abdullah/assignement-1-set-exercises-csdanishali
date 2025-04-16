2f2fe3ae7b85e4bdff85d362e6118b6b18f8d9b2
Abdullah Mubashir <abdullahmubashir009@gmail.com>
2025-04-16 22:58:35 +0400

    added comments

--- Chapter-5-Embedded Content/Exercises/Bonus Exercise/script.js
+++ Chapter-5-Embedded Content/Exercises/Bonus Exercise/script.js
@@ -1,9 +1,17 @@
+// Select all the boxes buttons on the page
 const buttons = document.querySelectorAll('.boxes');
 
+// Loop through each button and add a click event
 buttons.forEach(function(button) {
   button.addEventListener('click', function() {
+
+    // Get the audio file name from the button's data attribute
     const file = button.getAttribute('data-audio');
+
+    // Create a new Audio object with the file path
     const audio = new Audio(file);
+
+    // Play the audio
     audio.play();
   });
 });
